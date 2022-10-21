a6fc220e6a441a9cde45a7935262ab4f450294d0
Kefa698 <kefaombongi07@gmail.com>
2022-10-21 15:37:45 +0300

    first commit

---
+++ contracts/Lending.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Lending {
+    constructor() {}
+}
