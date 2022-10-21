6afe276d12f25f99cc289dbe4cf28573a04726a2
Kefa698 <kefaombongi07@gmail.com>
2022-10-21 15:40:00 +0300

    mappings

--- contracts/Lending.sol
+++ contracts/Lending.sol
@@ -7,5 +7,18 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Lending {
+    mapping(address => address) public s_tokenToPriceFeed;
+    address[] public s_allowedTokens;
+    // Account -> Token -> Amount
+    mapping(address => mapping(address => uint256)) public s_accountToTokenDeposits;
+    // Account -> Token -> Amount
+    mapping(address => mapping(address => uint256)) public s_accountToTokenBorrows;
+
+    // 5% Liquidation Reward
+    uint256 public constant LIQUIDATION_REWARD = 5;
+    // At 80% Loan to Value Ratio, the loan can be liquidated
+    uint256 public constant LIQUIDATION_THRESHOLD = 80;
+    uint256 public constant MIN_HEALH_FACTOR = 1e18;
+
     constructor() {}
 }
