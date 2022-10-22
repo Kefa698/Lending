7230f98a3186920b6fe43cbbc16d0f732038c4d6
Kefa698 <kefaombongi07@gmail.com>
2022-10-22 22:08:58 +0300

    lending

--- contracts/Lending.sol
+++ contracts/Lending.sol
@@ -19,7 +19,19 @@ contract Lending is ReentrancyGuard, Ownable {
     // At 80% Loan to Value Ratio, the loan can be liquidated
     uint256 public constant LIQUIDATION_THRESHOLD = 80;
     uint256 public constant MIN_HEALH_FACTOR = 1e18;
+
+    event AllowedTokenSet(address indexed token, address indexed priceFeed);
     event Deposit(address indexed account, address indexed token, uint256 indexed amount);
+    event Borrow(address indexed account, address indexed token, uint256 indexed amount);
+    event Withdraw(address indexed account, address indexed token, uint256 indexed amount);
+    event Repay(address indexed account, address indexed token, uint256 indexed amount);
+    event Liquidate(
+        address indexed account,
+        address indexed repayToken,
+        address indexed rewardToken,
+        uint256 halfDebtInEth,
+        address liquidator
+    );
 
     function deposit(address token, uint256 amount)
         external
@@ -30,6 +42,80 @@ contract Lending is ReentrancyGuard, Ownable {
         s_accountToTokenDeposits[msg.sender][token] += amount;
         bool success = IERC20(token).transferFrom(msg.sender, address(this), amount);
         require(success, "deposit failed");
+    }
+
+    function withdraw(address token, uint256 amount) external nonReentrant moreThanZero(amount) {
+        require(s_accountToTokenDeposits[msg.sender][token] >= amount, "not enough funds");
+        emit Withdraw(msg.sender, token, amount);
+        _pullFunds(msg.sender, token, amount);
+        require(healthfactor(msg.sender) >= MIN_HEALH_FACTOR, "Platform will go insolvent!");
+    }
+
+    function _pullFunds(
+        address account,
+        address token,
+        uint256 amount
+    ) private {
+        require(s_accountToTokenDeposits[account][token] >= amount, "not eneogh funds");
+        bool success = IERC20(token).transfer(msg.sender, amount);
+        require(success, "transfer failed");
+    }
+
+    function getAccountInformation(address user)
+        public
+        view
+        returns (uint256 borrowedValueInETH, uint256 collateralValueInETH)
+    {
+        borrowedValueInETH = getAccountBorrowedValue(user);
+        collateralValueInETH = getAccountCollateralValue(user);
+    }
+
+    function getAccountCollateralValue(address user) public view returns (uint256) {
+        uint256 totalCollateralValueInETH = 0;
+        for (uint256 index = 0; index < s_allowedTokens.length; index++) {
+            address token = s_allowedTokens[index];
+            uint256 amount = s_accountToTokenDeposits[user][token];
+            uint256 valueInEth = getEthValue(token, amount);
+            totalCollateralValueInETH += valueInEth;
+        }
+        return totalCollateralValueInETH;
+    }
+
+    function getAccountBorrowedValue(address user) public view returns (uint256) {
+        uint256 totalBorrowsValueInETH = 0;
+        for (uint256 index = 0; index < s_allowedTokens.length; index++) {
+            address token = s_allowedTokens[index];
+            uint256 amount = s_accountToTokenBorrows[user][token];
+            uint256 valueInEth = getEthValue(token, amount);
+            totalBorrowsValueInETH += valueInEth;
+        }
+        return totalBorrowsValueInETH;
+    }
+
+    function getEthValue(address token, uint256 amount) public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_tokenToPriceFeed[token]);
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        // 2000 DAI = 1 ETH
+        // 0.002 ETH per DAI
+        // price will be something like 20000000000000000
+        // So we multiply the price by the amount, and then divide by 1e18
+        // 2000 DAI * (0.002 ETH / 1 DAI) = 0.002 ETH
+        // (2000 * 10 ** 18) * ((0.002 * 10 ** 18) / 10 ** 18) = 0.002 ETH
+        return (uint256(price) * amount) / 1e18;
+    }
+
+    function getTokenValueFromEth(address token, uint256 amount) public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_tokenToPriceFeed[token]);
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return (amount * 1e18) / uint256(price);
+    }
+
+    function healthfactor(address account) public view returns (uint256) {
+        (uint256 borrowedValueInEth, uint256 collateralValueInEth) = getAccountInformation(account);
+        uint256 collateralAdjustedForThreshold = (collateralValueInEth * LIQUIDATION_THRESHOLD) /
+            100;
+        if (borrowedValueInEth == 0) return 100e18;
+        return (collateralAdjustedForThreshold * 1e18) / borrowedValueInEth;
     }
 
     /********************/
